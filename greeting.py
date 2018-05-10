ed3e1e584a37cef1e908af9fd7c54e9ea784d380
jingoldby <jjingoldby@gmail.com>
2018-05-09 17:05:04 -0700

    public chat id

--- greeting.py
+++ greeting.py
@@ -7,7 +7,7 @@ class GreetingBot:
 
     def greet(self):
         bot = telegram.Bot(os.environ["TELEGRAM_BOT_TOKEN"])
-        message = bot.send_message(chat_id='-249634021',text="Welcome to <b>FansUnite</b> - The Protocol for Sports Betting Applications. Check out our <a href='https://fansunite.io'>Website</a> for more details. \n\n Please note the core team all live in Vancouver PST, we'll get to your questions as soon as we're awake :)",parse_mode=telegram.ParseMode.HTML,disable_web_page_preview=True)
+        message = bot.send_message(chat_id='-1001098649642',text="Welcome to <b>FansUnite</b> - The Protocol for Sports Betting Applications. Check out our <a href='https://fansunite.io'>Website</a> for more details. \n\n Please note the core team all live in Vancouver PST, we'll get to your questions as soon as we're awake :)",parse_mode=telegram.ParseMode.HTML,disable_web_page_preview=True)
 
 if __name__ == '__main__':
     c = GreetingBot()
