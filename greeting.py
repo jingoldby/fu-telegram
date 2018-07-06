2e3f23f9a54425546dcc31159c89fffffbc15508
jingoldby <jjingoldby@gmail.com>
2018-07-05 19:21:51 -0700

    update greeting to include demo and every 2 days now

--- greeting.py
+++ greeting.py
@@ -1,5 +1,6 @@
 from telegram.ext import Updater, CommandHandler
 import telegram
+import datetime
 import os
 
 
@@ -7,9 +8,10 @@ class GreetingBot:
 
     def greet(self):
         bot = telegram.Bot(os.environ["TELEGRAM_BOT_TOKEN"])
-        message = bot.send_message(chat_id='-1001098649642',text="Welcome to <b>FansUnite</b> - The Protocol for Sports Betting Applications. Check out our <a href='https://fansunite.io'>Website</a> for more details. \n\nPlease note the core team all reside in Vancouver PST, we'll get to your questions as soon as we're awake :)",parse_mode=telegram.ParseMode.HTML,disable_web_page_preview=True)
+        message = bot.send_message(chat_id='-1001098649642',text="Welcome to <b>FansUnite</b> - The Protocol for Sports Betting Applications. Make sure to also check out our <a href='https://fansunite.io'>Website</a> and <a href='https://fansunite.bet'>Demo</a>. \n\nPlease note the core team all reside in Vancouver PST, we'll get to your questions as soon as we're awake :)",parse_mode=telegram.ParseMode.HTML,disable_web_page_preview=True)
 
 if __name__ == '__main__':
     c = GreetingBot()
-
-    c.greet()
+    day = datetime.date.today().strftime("%d")
+    if day % 2 == 0:
+        c.greet()
